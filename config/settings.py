d926a296429a1cced8b9e7b0e93e5b6b46c705e6
Mauricio <jhony.paredes7583@utc.edu.ec>
2026-06-23 12:39:35 -0500

    Actualizar encabezado de la pagina Sobre Nosotros

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Django', 
+        'NAME': 'django_quinto_bdd', 
         'USER': 'postgres',
         'PASSWORD': 'root', 
         'HOST': '127.0.0.1',
