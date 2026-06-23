83eb1d95d88c4312d609705a3e5f60ee8b03ace2
naylapallasco4912-droid <naylapallasco4912@utc.edu.ec>
2026-06-23 10:14:06 -0500

    Texto del buscador

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Django', 
         'USER': 'postgres',
-        'PASSWORD': 'root', 
+        'PASSWORD': '12345', 
         'HOST': '127.0.0.1',
         'PORT': '5432',
     } 
