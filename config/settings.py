667c0a5b1c9986d948af29cbc55820259cbe9d31
BRYANPULLUQUITIN <bryan.pulluquitin8715@utc.edu.ec>
2026-07-01 09:09:37 -0500

    Cambio de despcion en menu

--- config/settings.py
+++ config/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Django', 
+        'NAME': 'djangoquindo_db', 
         'USER': 'postgres',
-        'PASSWORD': '12345', 
+        'PASSWORD': 'root', 
         'HOST': '127.0.0.1',
         'PORT': '5432',
     } 
